af8c9544d70ba3e4a3bee5f1e1e38c40c4d5a26e
LUCIANO ALONSO PEREIRA RODRIGUEZ <l.a.pereirarodriguez@gmail.com>
2025-06-20 23:28:28 -0400

    .

---
+++ ferremas_api/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+import models.models as models
+
+app = Flask(__name__)
+
+@app.route('/productos', methods=['GET'])
+def listar_productos():
+    productos = models.listar_todos_los_productos()
+    return jsonify(productos)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='')
+    
