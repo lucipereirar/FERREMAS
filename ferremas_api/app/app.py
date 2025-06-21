d94d7b0bcc47a3d4a7a92bf5bb2cdf3f04089c14
LUCIANO ALONSO PEREIRA RODRIGUEZ <l.a.pereirarodriguez@gmail.com>
2025-06-21 12:57:57 -0400

    .

--- ferremas_api/app/app.py
+++ ferremas_api/app/app.py
@@ -1,16 +1,19 @@
 import os
 from flask import Flask, send_from_directory
 
-# Ruta absoluta al folder 'frontend'
+# Ruta absoluta a la carpeta frontend
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 STATIC_FOLDER = os.path.abspath(os.path.join(BASE_DIR, '..', 'frontend'))
 
+# Crear instancia de Flask con carpeta estática definida
 app = Flask(__name__, static_folder=STATIC_FOLDER, static_url_path='')
 
+# Ruta principal que sirve index.html
 @app.route("/")
 def root():
     return send_from_directory(STATIC_FOLDER, "index.html")
 
+# Ruta para archivos estáticos (JS, CSS, etc.)
 @app.route("/<path:filename>")
 def serve_static(filename):
     return send_from_directory(STATIC_FOLDER, filename)
--- ferremas_api/app/runapp.py
+++ ferremas_api/app/runapp.py
@@ -10,8 +10,6 @@ crear_tabla_productos()
 app.register_blueprint(productos_api)
 app.register_blueprint(contacto_api)
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-STATIC_FOLDER = os.path.abspath(os.path.join(BASE_DIR, '..', 'frontend'))
 
 if __name__ == "__main__":
     app.run(debug=True)
