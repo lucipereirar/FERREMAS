32fa1336020cfb8000dcfab9d263ebd4ad484b51
LUCIANO ALONSO PEREIRA RODRIGUEZ <l.a.pereirarodriguez@gmail.com>
2025-06-21 02:06:12 -0400

    .

--- ferremas_api/app/runapp.py
+++ ferremas_api/app/runapp.py
@@ -1,6 +1,9 @@
 from app.app import app  # ahora sí apunta correctamente
 from routes.productos import productos_api
 from routes.contacto import contacto_api
+from db.dbproductos import crear_tabla_productos, conectar_bd
+
+crear_tabla_productos()
 
 app.register_blueprint(productos_api)
 app.register_blueprint(contacto_api)
