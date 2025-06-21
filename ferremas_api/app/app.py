db2cef74e6524ccb71dc77168ef181141beaef61
LUCIANO ALONSO PEREIRA RODRIGUEZ <l.a.pereirarodriguez@gmail.com>
2025-06-21 12:16:31 -0400

    .

--- ferremas_api/app/app.py
+++ ferremas_api/app/app.py
@@ -2,15 +2,13 @@ import os
 from flask import Flask, send_from_directory
 
 # Ruta absoluta a la carpeta frontend
-static_folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'frontend'))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+static_folder_path = os.path.join(BASE_DIR, '..', 'frontend')
 
 app = Flask(__name__, static_folder=static_folder_path, static_url_path='')
 
-# Ruta raíz para servir index.html
 @app.route("/")
 def home():
-    print("Entró a /")
-    print("Ruta index:", os.path.join(static_folder_path, "index.html"))
     return send_from_directory(static_folder_path, "index.html")
 
 @app.route("/<path:filename>")
