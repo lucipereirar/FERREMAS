c00c70d1ee79813c501c898e1def77f69622e1cc
LUCIANO ALONSO PEREIRA RODRIGUEZ <l.a.pereirarodriguez@gmail.com>
2025-06-21 11:56:50 -0400

    .

--- ferremas_api/app/app.py
+++ ferremas_api/app/app.py
@@ -1,4 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 
-# Crear la aplicación Flask
-app = Flask(__name__)
+# Ruta absoluta al folder "frontend"
+STATIC_FOLDER_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "frontend"))
+
+app = Flask(__name__, static_folder=STATIC_FOLDER_PATH, static_url_path="")
+
+@app.route("/")
+def home():
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.route("/<path:filename>")
+def static_files(filename):
+    return send_from_directory(app.static_folder, filename)
